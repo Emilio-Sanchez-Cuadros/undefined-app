60b1511d3dc1bd260cd536c9ab09fad67cecc22d
Emilio <emilio@iloapps.dev>
2023-04-12 13:26:52 +0200

    Add log in link in title

--- ui/src/app/users/users.component.ts
+++ ui/src/app/users/users.component.ts
@@ -117,4 +117,26 @@ export class UsersComponent implements OnInit {
     })
   }
 
+  async login(action: string) {
+    console.log('login()', action);
+    const dialogRef = this.matDialog.open(DialogComponent, {
+      data: {
+        action
+      },
+    });
+    dialogRef.afterClosed().subscribe(async result => {
+      console.log('login data', result);
+      if (result) {
+        try {
+          this.token = await lastValueFrom(this._userService.login(result.user));
+          localStorage.setItem("token", this.token);
+          this._userService.setToken(this.token)
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      
+    })
+  }
+
 }
